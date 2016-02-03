d89cf0a429b89738adf4171eee7c422071eb089b
Conor C <Conorc1000@gmail.com>
2016-02-03 14:03:51 +0000

    added login component

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const Hello = () => {
-  return <div>Hello there</div>;
+const Login = () => {
+  return (
+
+      <form id="frmRegister" role="form">
+           <h2>Register</h2>
+
+          <div class="form-group">
+              <label for="txtRegEmail">Email address</label>
+              <input type="email" class="form-control" id="txtEmail" placeholder="Enter email" name="email" />
+          </div>
+          <div class="form-group">
+              <label for="txtRegPass">Password</label>
+              <input type="password" class="form-control" id="txtPass" placeholder="Password" name="password" />
+          </div>
+          <button type="submit" class="btn btn-default">Register</button>
+      </form>
+
+    )
   }
 
-export default Hello;
+export default Login;
